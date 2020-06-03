4d9481dbeed1c0cd0416da334ce36e4633f903ac
Fernando Daciuk <f.daciuk@gmail.com>
2020-06-03 07:17:40 -0300

    Fix symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ln -sf $PWD/alacritty/alacritty.yml ~/.alacritty.yml
 ln -sf $PWD/bin ~/bin
 ln -sf $PWD/conky ~/.config/conky
 ln -sf $PWD/nvim ~/.config/nvim
@@ -10,6 +9,7 @@ ln -sf $PWD/tmux/.tmux/.tmux.conf ~/.tmux.conf
 ln -sf $PWD/tmux/.tmux.conf.local ~/.tmux.conf.local
 
 ln -sf $PWD/zsh/.zshrc ~/.zshrc
-ln -sf $PWD/zsh/plugins ~/.oh-my-zsh/custom/plugins
+ln -sf $PWD/zsh/plugins/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+ln -sf $PWD/zsh/plugins/zsh-wakatime ~/.oh-my-zsh/custom/plugins/zsh-wakatime
 
 ln -sf $PWD/ssh/config ~/.ssh/config
